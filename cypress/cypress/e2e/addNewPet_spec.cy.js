f1872562c32ff1cd12773237dd34969f200f66a3
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-03-26 16:37:42 +0100

    test

--- cypress/cypress/e2e/addNewPet_spec.cy.js
+++ cypress/cypress/e2e/addNewPet_spec.cy.js
@@ -2,7 +2,7 @@ describe("Veterinarians Table", () => {
   it("successfully see the veterinaries", () => {
     cy.visit("https://prod.devopshub.org/");
 
-    cy.xpath("//body/layout-nav[1]/nav[1]/div[2]/ul[1]/li[3]/a[1]").click();
+    cy.get("//body/layout-nav[1]/nav[1]/div[2]/ul[1]/li[3]/a[1]").click();
 
     cy.get("table tbody").contains("td", "Helen Leary").should("exist");
   });
