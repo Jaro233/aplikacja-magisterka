bca1635e26fec1180894b472ef767486c613cea8
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-03-26 16:30:48 +0100

    test

--- cypress/cypress/e2e/addNewPet_spec.cy.js
+++ cypress/cypress/e2e/addNewPet_spec.cy.js
@@ -1,25 +1,9 @@
-describe("PetClinic Veterinaries Check", () => {
+describe("Veterinarians Table", () => {
   it("successfully see the veterinaries", () => {
-    // Step 1: Visit the homepage
     cy.visit("https://prod.devopshub.org/");
 
-    // Step 2: Navigate to Veterinaries page
-    // This step depends on the structure of your application.
-    // For instance, if there's a link to the form:
-    cy.xpath("//body/layout-nav[1]/nav[1]/div[2]/ul[1]/li[3]/a[1]").click(); // Adjust based on the actual text/link
+    cy.xpath("//body/layout-nav[1]/nav[1]/div[2]/ul[1]/li[3]/a[1]").click();
 
-    // Step 3: Fill in the form
-    // Replace the selectors with the actual form field identifiers
-    cy.get('input[name="name"]').type("Buddy");
-    cy.get('input[name="birthDate"]').type("2022-01-01");
-    cy.get('select[name="type"]').select("Dog");
-    cy.get('input[name="owner"]').type("John Doe");
-
-    // Step 4: Submit the form
-    cy.get("form").submit();
-
-    // Step 5: Verify the addition
-    // Adjust the verification step based on how your application confirms the addition
-    cy.contains("Pet Buddy has been added").should("be.visible");
+    cy.get("table tbody").contains("td", "Helen Leary").should("exist");
   });
 });
