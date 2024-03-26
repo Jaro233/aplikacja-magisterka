c4f5b866b0407d9659dca07697e9dd70accdeb2f
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-03-26 17:22:55 +0100

    test

--- scripts/checkSlot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-currentSlot=`(helm get values --all yourReleaseName | grep -Po '  productionSlot: \K.*')`
-if [ "$currentSlot" == "blue" ]; then
-    newSlot="green"
-else
-    newSlot="blue"
-fi
