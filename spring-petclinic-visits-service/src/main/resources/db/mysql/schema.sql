9c1b733fe03bad6ddab47b55d3423daa66a6a136
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-03-30 14:22:22 +0100

    fixed docker compose

--- spring-petclinic-visits-service/src/main/resources/db/mysql/schema.sql
+++ spring-petclinic-visits-service/src/main/resources/db/mysql/schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS service_instance_db;
+CREATE DATABASE IF NOT EXISTS petclinic;
 
-USE service_instance_db;
+USE petclinic;
 
 CREATE TABLE IF NOT EXISTS visits (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
