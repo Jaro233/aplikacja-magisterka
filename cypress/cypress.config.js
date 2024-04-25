a860147abd5ddef3a963beaaca0061614e215206
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-04-25 20:32:35 +0200

    test

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,6 +2,6 @@ const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://prod.devopshub.org",
+    baseUrl: "https://www.devopshub.org",
   },
 });
