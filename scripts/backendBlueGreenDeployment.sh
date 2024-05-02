a330cafebf5123108b77c09ff8f15236d1b5935f
Jaro233 <jaroslaw.waliczek@vp.pl>
2024-05-02 16:10:03 +0200

    test

--- scripts/backendBlueGreenDeployment.sh
+++ scripts/backendBlueGreenDeployment.sh
@@ -17,7 +17,7 @@ if [ "$DEPLOY_ONLY" == "true" ]; then
   # Deploy the new version (either blue or green) and wait for it to become ready
   helm upgrade ${APP_NAME_SHORT} ./kubernetes/helm/app/${APP_NAME_SHORT} --set microservice.image.repository_${newSlot}=${ECR_REGISTRY}/${ECR_REPO_NAME} --set microservice.image.tag_${newSlot}=1.${REPOSITORY_TAG} --reuse-values
 
-  if ! kubectl rollout status deployment/${APP_NAME_SHORT}-${newSlot} -n ${NAMESPACE} --timeout=180s; then
+  if ! kubectl rollout status deployment/${APP_NAME_SHORT}-${newSlot} -n ${NAMESPACE} --timeout=300s; then
     echo "Deployment failed. Starting rollback..."
     helm rollback "${APP_NAME_SHORT}"  # This rolls back to the previous revision
     exit 1
@@ -27,7 +27,7 @@ else
   # Change the connection string
   helm upgrade "${APP_NAME_SHORT}" ./kubernetes/helm/app/"${APP_NAME_SHORT}" --set database.uri_env_${newSlot}=mysql-prod.prod-db --set microservice.current_environment_${newSlot}=prod --set microservice.image.repository_${newSlot}=${ECR_REGISTRY}/${ECR_REPO_NAME} --set microservice.image.tag_${newSlot}=1.${REPOSITORY_TAG} --reuse-values
 
-  if ! kubectl rollout status deployment/${APP_NAME_SHORT}-${newSlot} -n ${NAMESPACE} --timeout=180s; then
+  if ! kubectl rollout status deployment/${APP_NAME_SHORT}-${newSlot} -n ${NAMESPACE} --timeout=300s; then
     echo "Deployment failed. Starting rollback..."
     CURRENT_REVISION=$(helm history ${APP_NAME_SHORT} --max 1 --output json | jq '.[0].revision')
 
@@ -62,7 +62,7 @@ else
   # Update the old version (oldSlot) to the stage configuration and wait for it to become ready
   helm upgrade "${APP_NAME_SHORT}" ./kubernetes/helm/app/"${APP_NAME_SHORT}" --set database.uri_env_${oldSlot}=mysql-stage.stage-db --set microservice.current_environment_${oldSlot}=stage --set microservice.image.repository_${oldSlot}=${ECR_REGISTRY}/${ECR_REPO_NAME} --set microservice.image.tag_${oldSlot}=1.${REPOSITORY_TAG} --reuse-values --reuse-values
   
-  if ! kubectl rollout status deployment/${APP_NAME_SHORT}-${oldSlot} -n ${NAMESPACE} --timeout=270s; then
+  if ! kubectl rollout status deployment/${APP_NAME_SHORT}-${oldSlot} -n ${NAMESPACE} --timeout=300s; then
     echo "Deployment failed. Starting rollback..."
     CURRENT_REVISION=$(helm history ${APP_NAME_SHORT} --max 1 --output json | jq '.[0].revision')
 
